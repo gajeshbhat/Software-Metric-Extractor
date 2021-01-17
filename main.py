c976f052384c11573c251e745f806880d39712da
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2021-01-16 21:29:13 -0500

    Added Database support to store the metrics

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-import hashlib
 import json
 import shutil
 from os import walk
 from pprint import pprint
+from metric_db import get_session, get_overall_obj, get_each_file_obj, ProjectMetric
 
 
 def get_all_py_files(dir_list, root_dir):
@@ -57,7 +57,8 @@ def get_analysis_data(projects_dirs):
         output_stream = os.popen("multimetric " + str(project_dir_path + "/*.py"))
         project_metric_data = output_stream.read()
         project_metric_list.append(
-            {'Project Path': project_dir_path,
+            {'project_name': str(project_dir_path).split('/')[1],
+             'Project Path': project_dir_path,
              'metric_data': str(project_metric_data).strip()
              }
         )
@@ -65,27 +66,56 @@ def get_analysis_data(projects_dirs):
     return project_metric_list
 
 
-def get_project_metric(project_metric_json):
+def insert_metric_db(each_project_metric, project_name):
+    db_session = get_session()
+    overall_metric_obj = get_overall_obj(each_project_metric['overall_metrics'], project_name)
+
+    db_session.add(overall_metric_obj)
+    db_session.commit()
+
+    project_saved_inst = db_session.query(ProjectMetric).filter_by(project_name=str(project_name)).first()
+
+    for file_metric in each_project_metric['file_metrics_list']:
+        each_file_obj = get_each_file_obj(file_metric,project_saved_inst)
+        db_session.add(each_file_obj)
+
+    db_session.commit()
+    db_session.close()
+
+
+def create_project_metric(project_metric_json):
     for x in project_metric_json:
         json_metric = json.loads(x["metric_data"])
         project_files_metric = json_metric["files"]
+
         overall_metrics = json_metric["overall"]
-        file_metircs_list = list()
+        mean_metric = json_metric['stats']['mean']
+        sd_metric = json_metric['stats']['sd']
+
+        file_metric_list = list()
         for key, value in project_files_metric.items():
-            file_metircs_list.append(value)
-        return {
+            if value != {}:
+                file_metric_list.append(value)
+        current_project_metric = {
             'overall_metrics': overall_metrics,
-            'file_metrics_list': file_metircs_list
+            'overall_mean': mean_metric,
+            'overall_sd': sd_metric,
+            'file_metrics_list': file_metric_list
         }
+        insert_metric_db(current_project_metric, x['project_name'])
 
 
 # Place the projects to analyse in Projects dir
 PROJECTS_DIR = "projects"
 ANALYSIS_DIR = "Analysis"
 
+# Insert table into database
 projects_to_crawl = get_dirs(PROJECTS_DIR)
 project_file_details = get_all_py_files(projects_to_crawl, PROJECTS_DIR)
 dirs_to_analyse = copy_analysis_files(project_file_details, ANALYSIS_DIR)
-# TODO: Integrate MongoDB and Clean the data and make it available for Analysis
 analysis_data = get_analysis_data(dirs_to_analyse)
-print(get_project_metric(analysis_data))
+# pprint(analysis_data)
+
+# TODO: Update the type or convert the json types to float in database object instantiation
+# Creates project metric
+create_project_metric(analysis_data)
--- metric_db.py
+++ metric_db.py
@@ -9,24 +9,36 @@ Base = declarative_base()
 
 class ProjectMetric(Base):
     __tablename__ = 'ProjectMetric'
+
+    # Relationships
+    files = relationship("FileCodeMetric",back_populates='project')
+
+    # Tables
     id = Column(Integer, primary_key=True)
+
     project_name = Column(String)
     comment_ratio = Column(Float)
     cyclomatic_complexity = Column(Integer)
+
     fanout_external = Column(Integer)
     fanout_internal = Column(Integer)
+
     halstead_bugprop = Column(Float)
     halstead_difficulty = Column(Float)
     halstead_effort = Column(Float)
     halstead_timerequired = Column(Float)
     halstead_volume = Column(Float)
+
     programming_lang = Column(String)
+
     lines_of_code = Column(Integer)
     maintainability_index = Column(Float)
+
     operands_sum = Column(Integer)
     operands_uniq = Column(Integer)
     operators_sum = Column(Integer)
     operators_uniq = Column(Integer)
+
     tiobe = Column(Float)
     tiobe_compiler = Column(Float)
     tiobe_complexity = Column(Float)
@@ -39,26 +51,37 @@ class ProjectMetric(Base):
 
 
 class FileCodeMetric(Base):
-    __tablename__ = 'EachFileMetric'
+    __tablename__ = 'FileCodeMetric'
+
+    # Relationships
+    project = relationship("ProjectMetric", back_populates='files')
+
+    # Table Columns
     id = Column(Integer, primary_key=True)
     project_id = Column(Integer, ForeignKey('ProjectMetric.id'))
-    ProjectMetric = relationship(ProjectMetric)
+
     comment_ratio = Column(Float)
+
     cyclomatic_complexity = Column(Integer)
     fanout_external = Column(Integer)
     fanout_internal = Column(Integer)
+
     halstead_bugprop = Column(Float)
     halstead_difficulty = Column(Float)
     halstead_effort = Column(Float)
     halstead_timerequired = Column(Float)
     halstead_volume = Column(Float)
+
     programming_lang = Column(String)
+
     lines_of_code = Column(Integer)
     maintainability_index = Column(Float)
+
     operands_sum = Column(Integer)
     operands_uniq = Column(Integer)
     operators_sum = Column(Integer)
     operators_uniq = Column(Integer)
+
     tiobe = Column(Float)
     tiobe_compiler = Column(Float)
     tiobe_complexity = Column(Float)
@@ -74,64 +97,64 @@ class FileCodeMetric(Base):
 def get_overall_obj(metric_obj, project_name):
     overall_metric = ProjectMetric \
             (
-            project_name=project_name,
-            comment_ratio=metric_obj['comment_ratio'],
-            cyclomatic_complexity=metric_obj['cyclomatic_complexity'],
-            fanout_external=metric_obj['fanout_external'],
-            fanout_internal=metric_obj['fanout_internal'],
-            halstead_bugprop=metric_obj['halstead_bugprop'],
-            halstead_difficulty=metric_obj['halstead_difficulty'],
-            halstead_effort=metric_obj['halstead_effort'],
-            halstead_timerequired=metric_obj['halstead_timerequired'],
-            halstead_volume=metric_obj['halstead_volume'],
-            programming_lang=metric_obj['lang'],
-            lines_of_code=metric_obj['loc'],
-            maintainability_index=metric_obj['maintainability_index'],
-            operands_sum=metric_obj['operands_sum'],
-            operands_uniq=metric_obj['operands_uniq'],
-            operators_sum=metric_obj['operators_sum'],
-            operators_uniq=metric_obj['operators_uniq'],
-            tiobe=metric_obj['tiobe'],
-            tiobe_compiler=metric_obj['tiobe_compiler'],
-            tiobe_complexity=metric_obj['tiobe_complexity'],
-            tiobe_coverage=metric_obj['tiobe_coverage'],
-            tiobe_duplication=metric_obj['tiobe_duplication'],
-            tiobe_fanout=metric_obj['tiobe_fanout'],
-            tiobe_functional=metric_obj['tiobe_functional'],
-            tiobe_security=metric_obj['tiobe_security'],
-            tiobe_standard=metric_obj['tiobe_standard']
+            project_name=str(project_name),
+            comment_ratio=float(metric_obj['comment_ratio']),
+            cyclomatic_complexity=int(metric_obj['cyclomatic_complexity']),
+            fanout_external=int(metric_obj['fanout_external']),
+            fanout_internal=int(metric_obj['fanout_internal']),
+            halstead_bugprop=float(metric_obj['halstead_bugprop']),
+            halstead_difficulty=float(metric_obj['halstead_difficulty']),
+            halstead_effort=float(metric_obj['halstead_effort']),
+            halstead_timerequired=float(metric_obj['halstead_timerequired']),
+            halstead_volume=float(metric_obj['halstead_volume']),
+            programming_lang=str(metric_obj['lang']),
+            lines_of_code=int(metric_obj['loc']),
+            maintainability_index=float(metric_obj['maintainability_index']),
+            operands_sum=int(metric_obj['operands_sum']),
+            operands_uniq=int(metric_obj['operands_uniq']),
+            operators_sum=int(metric_obj['operators_sum']),
+            operators_uniq=int(metric_obj['operators_uniq']),
+            tiobe=float(metric_obj['tiobe']),
+            tiobe_compiler=float(metric_obj['tiobe_compiler']),
+            tiobe_complexity=float(metric_obj['tiobe_complexity']),
+            tiobe_coverage=float(metric_obj['tiobe_coverage']),
+            tiobe_duplication=float(metric_obj['tiobe_duplication']),
+            tiobe_fanout=float(metric_obj['tiobe_fanout']),
+            tiobe_functional=float(metric_obj['tiobe_functional']),
+            tiobe_security=float(metric_obj['tiobe_security']),
+            tiobe_standard=float(metric_obj['tiobe_standard'])
         )
     return overall_metric
 
 
-def get_each_file_obj(metric_obj, project_obj):
+def get_each_file_obj(metric_obj,project_inst):
     file_metric_obj = FileCodeMetric(
-        ProjectMetric=project_obj,
-        comment_ratio=metric_obj['comment_ratio'],
-        cyclomatic_complexity=metric_obj['cyclomatic_complexity'],
-        fanout_external=metric_obj['fanout_external'],
-        fanout_internal=metric_obj['fanout_internal'],
-        halstead_bugprop=metric_obj['halstead_bugprop'],
-        halstead_difficulty=metric_obj['halstead_difficulty'],
-        halstead_effort=metric_obj['halstead_effort'],
-        halstead_timerequired=metric_obj['halstead_timerequired'],
-        halstead_volume=metric_obj['halstead_volume'],
-        programming_lang=metric_obj['lang'],
-        lines_of_code=metric_obj['loc'],
-        maintainability_index=metric_obj['maintainability_index'],
-        operands_sum=metric_obj['operands_sum'],
-        operands_uniq=metric_obj['operands_uniq'],
-        operators_sum=metric_obj['operators_sum'],
-        operators_uniq=metric_obj['operators_uniq'],
-        tiobe=metric_obj['tiobe'],
-        tiobe_compiler=metric_obj['tiobe_compiler'],
-        tiobe_complexity=metric_obj['tiobe_complexity'],
-        tiobe_coverage=metric_obj['tiobe_coverage'],
-        tiobe_duplication=metric_obj['tiobe_duplication'],
-        tiobe_fanout=metric_obj['tiobe_fanout'],
-        tiobe_functional=metric_obj['tiobe_functional'],
-        tiobe_security=metric_obj['tiobe_security'],
-        tiobe_standard=metric_obj['tiobe_standard']
+        comment_ratio=float(metric_obj['comment_ratio']),
+        cyclomatic_complexity=int(metric_obj['cyclomatic_complexity']),
+        fanout_external=int(metric_obj['fanout_external']),
+        fanout_internal=int(metric_obj['fanout_internal']),
+        halstead_bugprop=float(metric_obj['halstead_bugprop']),
+        halstead_difficulty=float(metric_obj['halstead_difficulty']),
+        halstead_effort=float(metric_obj['halstead_effort']),
+        halstead_timerequired=float(metric_obj['halstead_timerequired']),
+        halstead_volume=float(metric_obj['halstead_volume']),
+        programming_lang=str(metric_obj['lang']),
+        lines_of_code=int(metric_obj['loc']),
+        maintainability_index=float(metric_obj['maintainability_index']),
+        operands_sum=int(metric_obj['operands_sum']),
+        operands_uniq=int(metric_obj['operands_uniq']),
+        operators_sum=int(metric_obj['operators_sum']),
+        operators_uniq=int(metric_obj['operators_uniq']),
+        tiobe=float(metric_obj['tiobe']),
+        tiobe_compiler=float(metric_obj['tiobe_compiler']),
+        tiobe_complexity=float(metric_obj['tiobe_complexity']),
+        tiobe_coverage=float(metric_obj['tiobe_coverage']),
+        tiobe_duplication=float(metric_obj['tiobe_duplication']),
+        tiobe_fanout=float(metric_obj['tiobe_fanout']),
+        tiobe_functional=float(metric_obj['tiobe_functional']),
+        tiobe_security=float(metric_obj['tiobe_security']),
+        tiobe_standard=float(metric_obj['tiobe_standard']),
+        project=project_inst
     )
     return file_metric_obj
 
@@ -141,5 +164,5 @@ def get_session():
     Base.metadata.create_all(engine)
     Base.metadata.bind = engine
     metric_db_session = sessionmaker(bind=engine)
-    metric_db_session = metric_db_session() # sessionmaker returns a class for some fu**ing reason so instansiate again. Raise a pull request
+    metric_db_session = metric_db_session()  # sessionmaker returns a class for some fu**ing reason so instansiate again. Raise a pull request
     return metric_db_session
